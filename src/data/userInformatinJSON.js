88a3b809eb652973c73ad00a6200004a783440d8
İLKNUR DONDURMA <dondurmailknur001@gmail.com>
2024-10-15 15:30:39 +0300

    Update userInformatinJSON.js

--- src/data/userInformatinJSON.js
+++ src/data/userInformatinJSON.js
@@ -24,7 +24,7 @@ export const userInformationJSON=
     ],
     skills: [
       { "name": "React", "icon": "FaReact", "color": "#61DBFB" },
-      { "name": "ASP.NET Core", "icon": "SiDotnet", "color": "#512BD4" },
+      { "name": ".NET Core", "icon": "SiDotnet", "color": "#512BD4" },
       { "name": "Flutter", "icon": "SiFlutter", "color": "#02569B" },
       { "name": "HTML5", "icon": "FaHtml5", "color": "#E34F26" },
       { "name": "CSS3", "icon": "FaCss3Alt", "color": "#1572B6" },
